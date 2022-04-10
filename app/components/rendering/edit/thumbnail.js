ee4e3addccd60cec14d3a26d33f92c8278ca3b72
Aad Versteden <madnificent@gmail.com>
2022-04-10 14:56:29 +0200

    Allow setting thumbnail

---
+++ app/components/rendering/edit/thumbnail.js
@@ -0,0 +1,19 @@
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+
+export default class RenderingEditThumbnailComponent extends Component {
+  @service store;
+
+  @action
+  async uploadThumbnail(file) {
+    const result = await file.upload("/files");
+    this.store.pushPayload('file', result.body);
+    const uploadedFile = this.store.peekRecord('file', result.body.data.id);
+    const product = this.args.resource;
+    product.thumbnail = uploadedFile;
+    await product.save();
+    this.args.exitEditing();
+
+  }
+}
