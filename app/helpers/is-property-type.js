f1e627143b008107c1367844cea641b9743a2aec
Aad Versteden <madnificent@gmail.com>
2022-05-21 12:19:35 +0200

    Make property-type cope with receiving either class or resource
    We are encountering cases where we want to supply the prototype
    separately because we don't want to instantiate the instance but do want
    it to behave as if it were of a certain type.

--- app/helpers/is-property-type.js
+++ app/helpers/is-property-type.js
@@ -1,8 +1,17 @@
 import { helper } from '@ember/component/helper';
 import propertyType from 'freddie/utils/property-type';
 
+/**
+ * Yields the type of the supplied property.
+ *
+ * Either a class or a model must be supplied.
+ *
+ * @param {Object} options.class Class on which the property is defined.
+ * @param {Object} options.model Model on which the property is defined.
+ * @param {string} options.property The property of which we want to know the type
+ */
 export default helper(function isPropertyType(
-  [model, property, type] /*, named*/
+  type, options
 ) {
-  return propertyType(model, property) === type;
+  return propertyType(options) === type;
 });
--- app/utils/property-type.js
+++ app/utils/property-type.js
@@ -1,4 +1,13 @@
-export default function propertyType(model, property) {
-  const ctr = model.constructor;
-  return ctr && ctr.knownProperties && ctr.knownProperties[property];
+/**
+ * Yields the type of the supplied property.
+ *
+ * Either a class or a model must be supplied.
+ *
+ * @param {Object} options.class Class on which the property is defined.
+ * @param {Object} options.model Model on which the property is defined.
+ * @param {string} options.property The property of which we want to know the type
+ */
+export default function propertyType(options) {
+  const ctr = ( options.class || options.model.constructor );
+  return ctr && ctr.knownProperties && ctr.knownProperties[options.property];
 }
