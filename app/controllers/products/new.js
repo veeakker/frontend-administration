44a73ad7692a82682722b28607c4ebea1e3c97e0
Aad Versteden <madnificent@gmail.com>
2023-02-20 13:38:15 +0100

    Ensure unitPrice and targetUnit are set on Product.new
    When a product is created, we expect the unitPrice and the targetUnit to
    be created too.  The fastest way of making this happen is to create
    these in the frontend.

--- app/controllers/products/new.js
+++ app/controllers/products/new.js
@@ -9,8 +9,13 @@ export default class ProductsNewController extends Controller {
   @action
   async persist() {
     const { plu, label, description, sortIndex } = this.model.product;
+    const unitPrice = this.store.createRecord('unit-price-specification');
+    const targetUnit = this.store.createRecord('quantitative-value');
+    await unitPrice.save();
+    await targetUnit.save();
     const model = this.store.createRecord('product', {
-      plu, label, description, sortIndex, isEnabled: false
+      plu, label, description, sortIndex, isEnabled: false,
+      unitPrice, targetUnit
     });
     await model.save();
     this.router.transitionTo(model.constructor.linkOptions.edit, model);
