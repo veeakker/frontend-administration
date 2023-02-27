55fc7728fe387fa5e76171298afcbc556792e52a
Aad Versteden <madnificent@gmail.com>
2023-02-27 08:53:44 +0000

    Released v0.1.0

--- app/models/account.js
+++ app/models/account.js
@@ -2,8 +2,6 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class AccountModel extends Model {
   @attr() email;
-  @attr() firstName;
-  @attr() lastName;
-
   @belongsTo() postalAddress;
+  @belongsTo() person;
 }
