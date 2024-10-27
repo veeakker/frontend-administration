cc1ae6353fa134effaa53bd469efc6d436e846ac
Aad Versteden <madnificent@gmail.com>
2024-10-27 10:48:20 +0100

    Provide logout button
    When users are logged in to the admin system, we can now present them
    with a logout button too.

---
+++ app/controllers/application.js
@@ -0,0 +1,15 @@
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import Controller from '@ember/controller';
+
+export default class ApplicationController extends Controller {
+  @service session;
+  @service router;
+
+  @action
+  async logout() {
+    await this.session.setup();
+    await this.session.invalidate();
+    this.router.transitionTo("login");
+  }
+}
