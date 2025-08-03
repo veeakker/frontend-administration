d8777385972b7aea28403bfa6397ecba259ef2bf
Aad Versteden <madnificent@gmail.com>
2025-08-03 22:29:48 +0200

    Fix editing of products within product group
    This is from the product-groups view.  Editing from the product's side should be
    operational.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,16 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
+  async findHasManyWithPageSize(_store, _snapshot, url, _relationship) {
+    // Append or merge query params to the URL
+    const separator = url.includes('?') ? '&' : '?';
+    url += separator + "page[size]=750";
+
+    return this.ajax(url, 'GET');
+  }
+
   async findHasMany() {
-    const response = await super.findHasMany(...arguments);
+    const response = await this.findHasManyWithPageSize(...arguments);
     if ( response.meta.count > response.data.length ) {
       console.warn("Not all resources for findHasMany returned from backend");
     }
--- app/components/rendering/edit/products.js
+++ app/components/rendering/edit/products.js
@@ -3,6 +3,7 @@ import { A } from '@ember/array';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
+import { trackedFunction } from 'reactiveweb/function';
 
 export default class RenderingEditProductsComponent extends Component {
   @tracked products = A()
@@ -11,27 +12,30 @@ export default class RenderingEditProductsComponent extends Component {
   @action
   async startSearch(search) {
     const filter = search ? { filter: { label: search } } : {};
-    this.products = (await this.store.query('product', filter)).toArray();
+    this.products = await this.store.query('product', filter);
   }
 
+  newProductsTF = trackedFunction(this, async () => {
+    const products = this.products;
+    const selectedProducts = [...await get(this.args.resource, this.args.property)];
+    return products.filter((x) => ! selectedProducts.includes(x));
+  })
+
   get newProducts() {
-    return this.products
-      .filter((x) =>
-        ! get(this.args.resource, this.args.property)
-          .toArray()
-          .includes(x));
+    return this.newProductsTF?.value;
   }
 
   @action
   async add(product) {
-    (await get(this.args.resource, this.args.property)).pushObject(product);
+    const arr = [...(await get(this.args.resource, this.args.property))];
+    arr.push(product);
+    set( this.args.resource, this.args.property, arr );
   }
 
   @action
   async remove(product) {
     const currentProducts = await get(this.args.resource, this.args.property);
     const newProducts = currentProducts
-          .toArray()
           .filter( (x) => x.id != product.id );
     set( this.args.resource, this.args.property, newProducts );
   }
