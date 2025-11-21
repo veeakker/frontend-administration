46624112bc5a776eacfb146c706322ee14f0d819
Aad Versteden <madnificent@gmail.com>
2025-11-21 23:28:07 +0100

    Allow indication of product groups which shouldn't be rendered.

--- app/models/business-entity.js
+++ app/models/business-entity.js
@@ -8,6 +8,12 @@ export default class BusinessEntityModel extends Model {
   @string() email;
   @text() description;
   @hasMany('delivery-place', { inverse: "businessEntity" }) deliveryPlaces;
+  @hasMany('product-group', {
+    inverse: null,
+    show: 'rendering/show/product-groups',
+    edit: 'rendering/edit/product-groups',
+    inverse: null
+  }) disallowedProductGroups;
 
   get label() {
     return this.name;
